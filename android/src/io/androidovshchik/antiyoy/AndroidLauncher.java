eaad7463a815632490d04bba966fe790d98b04fe
vlad <gimrcpp@gmail.com>
2018-08-03 20:18:18 +0300

    init

---
+++ android/src/io/androidovshchik/antiyoy/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package io.androidovshchik.antiyoy;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import io.androidovshchik.antiyoy.MyGdxGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyGdxGame(), config);
+	}
+}
